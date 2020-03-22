01ca093e0681414e709f0671b34400fcb47d5d15
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-03-22 21:53:11 +0000

    Fauna?

--- api/symbols/[symbol].js
+++ api/symbols/[symbol].js
@@ -1,7 +1,22 @@
 import symbols from '../_data/symbols.json'
+import faunadb from 'faunadb'
+const q = faunadb.query
+const dbClient = new faunadb.Client({ secret: process.env.FAUNA_SECRET_KEY })
 
 export default (request, response) => {
     const input = request.query.symbol.toUpperCase()
+
+    try {
+        dbClient.query(
+            q.Match(q.Index('get_by_symbol'), 'ABMD')
+          )
+          .then(res => console.log(res))
+          .catch(err => console.log(err))
+     } catch(err) {
+        console.log(err)
+        response.status(500).json(err) 
+    }
+
     const symbol = symbols.find(item => item.symbol === input)
 
     if(symbol) {
