abcd263b168751d7c70b8794fc43f56933a9ed48
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-03-25 23:00:37 +0000

    api structure change

--- api/stock.js
+++ api/stock.js
@@ -1,31 +1,35 @@
 import fetch from 'node-fetch'
 require('dotenv').config()
 
+const getTimestampFromDateStr = (dt) => {
+    Math.floor(new Date(dt).getTime() / 1000)
+}
+
 export default async (request, response) => {
-    const endpoint = 'https://finnhub.io/api/v1/stock/candle'
-    const symbols = request.query.symbols.split(',')
+    const API_ENDPOINT = 'https://finnhub.io/api/v1/stock/candle'
+    const symbolList = request.query.symbolList.split(',')
 
-    if (symbols && symbols.length > 0) {
+    if (symbolList && symbolList.length > 0) {
         const API_KEY = process.env.FINNHUB_API_KEY
-        const firstDayOf2020 = '01/02/2020'
+        const FIRST_DAY_2020 = '01/02/2020'
 
-        let numberOfDays = 0
-
-        const promises = symbols.map((symbol) => {
-            const from = Math.floor(new Date(firstDayOf2020).getTime() / 1000)
-            const to = Math.floor(new Date().getTime() / 1000)
+        const promises = symbolList.map((symbol) => {
+            const from = Math.floor(getTimestampFromDateStr(FIRST_DAY_2020))
+            const to = Math.floor(getTimestampFromDateStr())
 
             return fetch(
-                `${endpoint}?symbol=${symbol.toUpperCase()}&resolution=D&from=${from}&to=${to}&token=${API_KEY}`
+                `${API_ENDPOINT}?symbol=${symbol.toUpperCase()}&resolution=D&from=${from}&to=${to}&token=${API_KEY}`
             ).then((res) => res.json())
         })
 
         const results = await Promise.all(promises)
+
         let data = []
+        let dates = new Set()
 
         results.map((res, idx) => {
             const { c, o, h, l, t, s } = res
-            const symbol = symbols[idx]
+            const symbol = symbolList[idx]
 
             if (s !== 'no_data') {
                 data.push({
@@ -33,15 +37,14 @@ export default async (request, response) => {
                     open: o,
                     high: h,
                     low: l,
-                    time: t,
                     symbol,
                 })
 
-                numberOfDays = t.length
+                dates.add(t)
             }
         })
 
-        response.status(200).json({ series: data, days: numberOfDays })
+        response.status(200).json({ series: data, labels: dates })
     }
 
     response.status(400)
