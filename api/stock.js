b0cdfac3ce3044003a133a4bfe5fe694397aa52d
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-03-28 16:10:59 +0000

    Linting fixed

--- api/stock.js
+++ api/stock.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 require('dotenv').config()
 
-const getTimestampFromDateStr = (dt) => Math.floor(dt.getTime() / 1000)
+const getTimestampFromDateStr = dt => Math.floor(dt.getTime() / 1000)
 
 export default async (request, response) => {
     const API_ENDPOINT = 'https://finnhub.io/api/v1/stock/candle'
@@ -11,13 +11,13 @@ export default async (request, response) => {
         const API_KEY = process.env.FINNHUB_API_KEY
         const FIRST_DAY_2020 = '01/02/2020'
 
-        const promises = symbolList.map((symbol) => {
+        const promises = symbolList.map(symbol => {
             const from = getTimestampFromDateStr(new Date(FIRST_DAY_2020))
             const to = getTimestampFromDateStr(new Date())
 
             return fetch(
                 `${API_ENDPOINT}?symbol=${symbol.toUpperCase()}&resolution=D&from=${from}&to=${to}&token=${API_KEY}`
-            ).then((res) => res.json())
+            ).then(res => res.json())
         })
 
         const results = await Promise.all(promises)
