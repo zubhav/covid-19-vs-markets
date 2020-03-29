cf84434397388129f16f1131257c49708dbf187f
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-03-29 00:33:12 +0000

    initial loader no ?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -283,7 +283,7 @@
                         class="bg-gray-300 flex items-center p-2 h-36"
                         aria-label="Empty symbol slot">
                         <p class="text-center text-6xl text-gray-600 w-full">
-                            {#if loading}
+                            {#if loading || dates.length === 0}
                                 <Loader />
                             {:else}?{/if}
                         </p>
