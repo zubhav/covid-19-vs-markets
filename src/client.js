53b17a2bba4d53f3d353794d038b08a03f18a6a6
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-03-31 00:34:49 +0100

    grid

--- src/client.js
+++ src/client.js
@@ -5,6 +5,5 @@ sapper.start({
     target: document.querySelector('#sapper'),
 })
 
-console.log(process.env.NODE_ENV)
 if (process.env.TRACKING_ID && process.env.NODE_ENV === 'production')
     ga(process.env.TRACKING_ID)
