0ea12175e4ca37a1559dd5cbe1d1e5559365bbcf
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-03-30 21:58:25 +0100

    Analytics

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,8 @@ import { terser } from 'rollup-plugin-terser'
 import config from 'sapper/config/rollup.js'
 import pkg from './package.json'
 
+require('dotenv').config()
+
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
 const legacy = !!process.env.SAPPER_LEGACY_BUILD
@@ -24,6 +26,9 @@ export default {
             replace({
                 'process.browser': true,
                 'process.env.NODE_ENV': JSON.stringify(mode),
+                'process.env.TRACKING_ID': JSON.stringify(
+                    process.env.TRACKING_ID || ''
+                ),
             }),
 
             svelte({
@@ -79,6 +84,9 @@ export default {
             replace({
                 'process.browser': false,
                 'process.env.NODE_ENV': JSON.stringify(mode),
+                'process.env.TRACKING_ID': JSON.stringify(
+                    process.env.TRACKING_ID || ''
+                ),
             }),
 
             svelte({
@@ -108,6 +116,9 @@ export default {
             replace({
                 'process.browser': true,
                 'process.env.NODE_ENV': JSON.stringify(mode),
+                'process.env.TRACKING_ID': JSON.stringify(
+                    process.env.TRACKING_ID || ''
+                ),
             }),
             commonjs(),
             !dev && terser(),
---
+++ src/utils/ga.js
@@ -0,0 +1,14 @@
+export const ga = gaID => {
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+        window.dataLayer.push(arguments)
+    }
+
+    gtag('js', new Date())
+
+    gtag('config', gaID)
+
+    const script = document.createElement('script')
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${gaID}`
+    document.body.appendChild(script)
+}
