d056d46943fd8d7fc4af01f45da9fdda277c6900
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-03-30 22:46:37 +0100

    Now.json change

--- src/client.js
+++ src/client.js
@@ -5,6 +5,6 @@ sapper.start({
     target: document.querySelector('#sapper'),
 })
 
-console.log(process.env.TRACKING_ID)
+console.log(process.env.NODE_ENV)
 if (process.env.TRACKING_ID && process.env.NODE_ENV === 'production')
     ga(process.env.TRACKING_ID)
