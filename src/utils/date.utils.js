dd753256115fd5fede46ecfd9a858860cfb74429
Subhav Gautam <9058689+zubhav@users.noreply.github.com>
2020-04-02 22:53:24 +0100

    query params

---
+++ src/utils/date.utils.js
@@ -0,0 +1,9 @@
+const padZero = num => (num < 10 ? `0${num}` : `${num}`)
+
+export const getDateFromTimestamp = timestamp => {
+    const dateObj = new Date(timestamp * 1000)
+    const year = dateObj.getFullYear()
+    const month = padZero(dateObj.getMonth() + 1)
+    const day = padZero(dateObj.getDate())
+    return `${day}/${month}/${year}`
+}
