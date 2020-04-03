41e9028726d794e1ab5bea030fb8e24280b5a46b
Pranjal <pjately078@gmail.com>
2020-04-03 23:08:07 +0100

    small change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,8 @@
     import { fetchFromApi } from '../utils/fetchFromApi.utils'
     import { getDateFromTimestamp } from '../utils/date.utils'
     import {
-        setLocalStorageItem,
         getLocalStorageItem,
+        setLocalStorageItem,
     } from '../utils/localStorage.utils'
     import Chart from '../components/chart.svelte'
     import Loader from '../components/loader.svelte'
--- src/utils/localStorage.utils.js
+++ src/utils/localStorage.utils.js
@@ -1,4 +1,4 @@
+export const getLocalStorageItem = key => localStorage.getItem(key)
+
 export const setLocalStorageItem = (key, value) =>
     localStorage.setItem(key, value)
-
-export const getLocalStorageItem = key => localStorage.getItem(key)
